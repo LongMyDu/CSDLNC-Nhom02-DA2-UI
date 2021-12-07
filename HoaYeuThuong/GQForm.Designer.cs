f77bab77b39349f63b5f8ea2a929cb5431cb180d
Gia Quyen <giaquyen200174@gmail.com>
2021-12-07 22:35:57 +0700

    Connect to DB and fetch data

---
+++ HoaYeuThuong/GQForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace HoaYeuThuong
+{
+    partial class GQForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.grdData = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(26, 42);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 23);
+            this.ConnectButton.TabIndex = 0;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // grdData
+            // 
+            this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdData.Location = new System.Drawing.Point(26, 86);
+            this.grdData.Name = "grdData";
+            this.grdData.RowHeadersWidth = 51;
+            this.grdData.RowTemplate.Height = 24;
+            this.grdData.Size = new System.Drawing.Size(738, 292);
+            this.grdData.TabIndex = 1;
+            // 
+            // GQForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grdData);
+            this.Controls.Add(this.ConnectButton);
+            this.Name = "GQForm";
+            this.Text = "FormGQ";
+            this.Load += new System.EventHandler(this.GQForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.DataGridView grdData;
+    }
+}
---
+++ HoaYeuThuong/GQForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HoaYeuThuong
+{
+    public partial class GQForm : Form
+    {
+        public GQForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = "Data Source=DESKTOP-MNUAD46\\SQLEXPRESS;Initial Catalog=DB_HoaYeuThuong;User ID=sa;Password=svcntt";
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                //retrieve the SQL Server instance version
+                string query = @"SELECT * FROM SANPHAMQUATANG";
+
+                //define the SqlCommand object
+                SqlCommand cmd = new SqlCommand(query, cnn);
+
+                //Set the SqlDataAdapter object
+                SqlDataAdapter dAdapter = new SqlDataAdapter(cmd);
+
+                //define dataset
+                DataSet ds = new DataSet();
+
+                //fill dataset with query results
+                dAdapter.Fill(ds);
+
+                //set DataGridView control to read-only
+                grdData.ReadOnly = true;
+
+                //set the DataGridView control's data source/data table
+                grdData.DataSource = ds.Tables[0];
+
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+
+        private void GQForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
